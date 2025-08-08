90fecd72f4938e0f76051c92c3b16cd08e077bc1
dnylng <leung.danny45@gmail.com>
2025-08-08 19:38:58 +1000

    doing extension

--- dleung-library/src/components/JSON.vue
+++ dleung-library/src/components/JSON.vue
@@ -43,6 +43,11 @@
       <p>{{ austen?.name }}'s works:</p>
       <!-- Activity 9: Render a list of Austen's works. Hint: Use the v-for directive to iterate through the array of authors that you have filtered out. -->
       <!-- TODO: CODE TO RENDER LIST OF AUSTEN'S WORKS HERE -->
+      <ul>
+        <li v-for="work in austen?.famousWorks" :key="work.title">
+          {{ work.title }} ({{ work.year }})
+        </li>
+      </ul>
     </section>
 
     <section class="lab-section">
@@ -54,6 +59,7 @@
         Company:
         <!-- Activity 9a: Get the company name from the bookstores object. -->
         <!-- TODO: CODE TO GET COMPANY NAME HERE -->
+        
       </p>
 
       <p>
@@ -122,11 +128,13 @@ const allFamousWorks = computed(() => {
 // Activity 4: Find author by name
 const orwell = computed(() => {
   // TODO: CODE TO FIND AUTHOR BY NAME HERE
+  return authors.find(author => author.name === "George Orwell")
 })
 
 // Activity 5: Find author by ID
 const austen = computed(() => {
   // TODO: CODE TO FIND AUTHOR BY ID HERE
+  return authors.find(author => author.id === 1)
 })
 </script>
 
